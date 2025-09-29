ae499f4c5115a66ad8320e2f13a6947e12cecb38
Gabriel <dev.gabriel2000@gmail.com>
2025-09-29 19:45:18 -0300

    cria função para lidar com o submit

--- app/_components/Form.js
+++ app/_components/Form.js
@@ -1,11 +1,18 @@
 
 // cria formulário para cadastrar uma nova task
 export default function Form() {
+    // função para lidar com o submit
+    function handleSubmit(event) {
+        const { preventDefault, target } = event;
+        preventDefault();
+    }
+    
     return(
         <div
             className="h-2/6 flex justify-center"
         >
             <form
+                onSubmit={ handleSubmit }
                 className="h-12"
             >
                 <input 
@@ -14,6 +21,7 @@ export default function Form() {
                 />
                 <button
                     className="border border-red-600 m-1 p-1.5 bg-red-500 text-white"
+                    type="submit"
                 >
                     Add task
                 </button>
