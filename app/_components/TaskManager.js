d4465708482192a83c5908ca99fa17f0232a89a0
Gabriel <dev.gabriel2000@gmail.com>
2025-09-29 21:45:58 -0300

    feat: As tarefas são salvas e recuperadas do local storage

--- app/_components/TaskManager.js
+++ app/_components/TaskManager.js
@@ -1,6 +1,6 @@
 'use client'
 // importa componentes filho
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./Form";
 import List from "./List";
 
@@ -8,6 +8,17 @@ import List from "./List";
 // cria componente container para gerir os componentes filhos
 export default function TaskManager() {
     const [tasks, setNewTask] = useState([]); // usa o hook react para criar um estado para armarzenar as novas tasks
+
+    useEffect(() => {
+        if(localStorage.getItem('tasks')) {
+            const taskList = localStorage.getItem('tasks');
+            setNewTask(JSON.parse(taskList));
+        }
+    },[]) // recupera se existir tasks salvas previamente na montagem do componente
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    },[tasks]) // ao alterar a lista de tasks salva no local storage a nova lista;
     function removeTask(taskToRemove) {
         const newList = tasks.filter((_, index) => index != taskToRemove); // filtra as tasks e remove pelo indice
         setNewTask(newList); // Seta a nova lista sem a task desejada
